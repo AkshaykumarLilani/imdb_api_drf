0009f093466827ff83d1f6d3c04bce913b3ab8e0
Akshay Lilani <akshaylilani60@gmail.com>
2023-03-01 10:00:17 +0530

    👌 IMPROVE: Add try catch to all routes

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from .serializers import MovieSerializer
@@ -16,40 +17,51 @@ def movie_list(request):
             # if you don't add many=True, then you will get a 'Got AttributeError when attempting to get a value for field `name` on serializer `MovieSerializer`.The serializer field might be named incorrectly and not match any attribute or key on the `QuerySet` instance. Original exception text was: 'QuerySet' object has no attribute 'name'.
         
         # 3. return the serializer.data to the response function from the rest framework which will automatically convert the native datatype to json
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     if request.method == 'POST':
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             # the serializer.data comes from def create method written in the serializer
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE']) # Default is GET request
 def movie_detail(request, movie_id):
     if request.method == 'GET':
-        movie = Movie.objects.get(pk=movie_id)
+        
+        try:
+            movie = Movie.objects.get(pk=movie_id)
+        except Movie.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
         serializer = MovieSerializer(movie)
             # You don't need to add many=True, because here we are accessing only one object not many objects at once.
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == 'PUT':
         #1. First fetch the movie which is to be updated.
-        movie = Movie.objects.get(pk=movie_id)
+        try:
+            movie = Movie.objects.get(pk=movie_id)
+        except Movie.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         #2. Pass the movie which is to be updated to the serializer and the new data to update with it. This will go to serializer and hit the update method which we wrote there and will bring whatever the update method returns.
         serializer = MovieSerializer(movie, data=request.data)
         #3. check if the serializer is valid and save if it is or send errors if it is not.
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == 'DELETE':
         #1. First fetch the movie which is to be deleted.
-        movie = Movie.objects.get(pk=movie_id)
+        try:
+            movie = Movie.objects.get(pk=movie_id)
+        except Movie.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         #2. Delete it!
         movie.delete()
         #3. send the response accordingly.
-        return Response(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
