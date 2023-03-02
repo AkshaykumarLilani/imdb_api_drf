447665fda5ee862441b91374867ea57d1fde10c1
Akshay Lilani <akshaylilani60@gmail.com>
2023-03-02 12:01:11 +0530

    👌 IMPROVE: Add custom serializer fields and convert to class based views

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,23 +1,69 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+# 3. validators
+def name_length(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("Name is too short")
+    else:
+        return value
     
-    def create(self, validated_data):
-        # this return is accessed in serializer.data when serializer.is_valid() returns True in the views
-        return Movie.objects.create(**validated_data)
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(validators=[name_length])
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+        
     
-    #instance carries the old values and validated_data carries the new values
-    def update(self, instance, validated_data):
-        # you have to match all the fields in the validated data with instance and see how the data is accessed from validated_data (we are giving a else field to return if the data is not present in the validated data (instance.name, etc.)).
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        # save it once you have updated all the fields.
-        instance.save()
-        #return the new data to the view
-        return instance
+#     def create(self, validated_data):
+#         # this return is accessed in serializer.data when serializer.is_valid() returns True in the views
+#         return Movie.objects.create(**validated_data)
+    
+#     #instance carries the old values and validated_data carries the new values
+#     def update(self, instance, validated_data):
+#         # you have to match all the fields in the validated data with instance and see how the data is accessed from validated_data (we are giving a else field to return if the data is not present in the validated data (instance.name, etc.)).
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         # save it once you have updated all the fields.
+#         instance.save()
+#         #return the new data to the view
+#         return instance
+
+#     # 1. Field level validation.
+#     # def validate_name(self, value):
+#     #     if len(value) < 2:
+#     #         raise serializers.ValidationError("Name is too short")
+#     #     else:
+#     #         return value
+    
+#     # 2. object level validation.
+#     def validate(self, data):
+#         if data["name"] == data["description"]:
+#             raise serializers.ValidationError("Name and description cannot be the same")
+#         return data
+
+class MovieSerializer(serializers.ModelSerializer):
+    name_len = serializers.SerializerMethodField()
+    class Meta:
+        model = Movie
+        fields = '__all__'
+        # fields = ['id','name','description'] # show name, id and description field
+		# exclude = ['name'] # show all excluding name
+  
+    def get_name_len(self, object):
+        length = len(object.name)
+        return length
+
+    # 1. Field level validation.
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is too short")
+        else:
+            return value
+    
+    # 2. object level validation.
+    def validate(self, data):
+        if data["name"] == data["description"]:
+            raise serializers.ValidationError("Name and description cannot be the same")
+        return data
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,14 +1,15 @@
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from watchlist_app.models import Movie
 from .serializers import MovieSerializer
 
-# Create your views here.
-@api_view(['GET', 'POST']) # GET request is the default one, i.e. if you only write @api_view(), then only GET will be accepted. And because I have written POST here too, I will see a post form when I hit the url to this function.
-def movie_list(request):
+class MovieListAV(APIView):
     
-    if request.method == 'GET':
+    #instead of using if else, we have functions of each request type to override.
+    
+    def get(self, request):
         # 1. Get the complex data
         movies = Movie.objects.all()
         
@@ -19,7 +20,7 @@ def movie_list(request):
         # 3. return the serializer.data to the response function from the rest framework which will automatically convert the native datatype to json
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    if request.method == 'POST':
+    def post(self, request):
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -27,9 +28,9 @@ def movie_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE']) # Default is GET request
-def movie_detail(request, movie_id):
-    if request.method == 'GET':
+class MovieDetailsAV(APIView):
+    
+    def get(self, request, movie_id):
         
         try:
             movie = Movie.objects.get(pk=movie_id)
@@ -40,7 +41,7 @@ def movie_detail(request, movie_id):
             # You don't need to add many=True, because here we are accessing only one object not many objects at once.
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    if request.method == 'PUT':
+    def put(self ,request, movie_id):
         #1. First fetch the movie which is to be updated.
         try:
             movie = Movie.objects.get(pk=movie_id)
@@ -55,7 +56,7 @@ def movie_detail(request, movie_id):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == 'DELETE':
+    def delete(self, request, movie_id):
         #1. First fetch the movie which is to be deleted.
         try:
             movie = Movie.objects.get(pk=movie_id)
@@ -65,3 +66,65 @@ def movie_detail(request, movie_id):
         movie.delete()
         #3. send the response accordingly.
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# # Create your views here.
+# @api_view(['GET', 'POST']) # GET request is the default one, i.e. if you only write @api_view(), then only GET will be accepted. And because I have written POST here too, I will see a post form when I hit the url to this function.
+# def movie_list(request):
+    
+#     if request.method == 'GET':
+#         # 1. Get the complex data
+#         movies = Movie.objects.all()
+        
+#         # 2. Convert the complex data to python native datatype
+#         serializer = MovieSerializer(movies, many=True) 
+#             # if you don't add many=True, then you will get a 'Got AttributeError when attempting to get a value for field `name` on serializer `MovieSerializer`.The serializer field might be named incorrectly and not match any attribute or key on the `QuerySet` instance. Original exception text was: 'QuerySet' object has no attribute 'name'.
+        
+#         # 3. return the serializer.data to the response function from the rest framework which will automatically convert the native datatype to json
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+#     if request.method == 'POST':
+#         serializer = MovieSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             # the serializer.data comes from def create method written in the serializer
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET', 'PUT', 'DELETE']) # Default is GET request
+# def movie_detail(request, movie_id):
+#     if request.method == 'GET':
+        
+#         try:
+#             movie = Movie.objects.get(pk=movie_id)
+#         except Movie.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+        
+#         serializer = MovieSerializer(movie)
+#             # You don't need to add many=True, because here we are accessing only one object not many objects at once.
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+#     if request.method == 'PUT':
+#         #1. First fetch the movie which is to be updated.
+#         try:
+#             movie = Movie.objects.get(pk=movie_id)
+#         except Movie.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         #2. Pass the movie which is to be updated to the serializer and the new data to update with it. This will go to serializer and hit the update method which we wrote there and will bring whatever the update method returns.
+#         serializer = MovieSerializer(movie, data=request.data)
+#         #3. check if the serializer is valid and save if it is or send errors if it is not.
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     if request.method == 'DELETE':
+#         #1. First fetch the movie which is to be deleted.
+#         try:
+#             movie = Movie.objects.get(pk=movie_id)
+#         except Movie.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         #2. Delete it!
+#         movie.delete()
+#         #3. send the response accordingly.
+#         return Response(status=status.HTTP_204_NO_CONTENT)
